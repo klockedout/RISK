f2930afb6bc41f7a654b82e00c2101f03275f652
WidadBelbekri <119508011+WidadBelbekri@users.noreply.github.com>
2023-10-27 08:48:32 +0200

    test methode dans le main

--- src/risk/Main.java
+++ src/risk/Main.java
@@ -3,7 +3,7 @@ package risk;
 import controler.Controler;
 import vue.Vue;
 import model.*;
-
+import model.Territoire;
 
 import java.util.ArrayList;
 
@@ -26,31 +26,32 @@ public class Main {
 	
 	
 
-    // Test de obtenirListeTerritoire
+    // Test obtenirListeTerritoire
     ArrayList<Territoire> listeTerritoires = model.obtenirListeTerritoire();
     for (Territoire territoire2 : listeTerritoires) {
         System.out.println("Territoire : " + territoire2.getNomTer());
     }
 
-    // Test de obtenirNbTerritoire
+    // Test obtenirNbTerritoire
     int nbTerritoires = model.obtenirNbTerritoire();
     System.out.println("Nombre de territoires : " + nbTerritoires);
 
-	// Test de getTerritoireDispo
-	Joueur joueur = new Joueur(2, "bleu", 4); // Créez un joueur ou utilisez un joueur existant
-	//ArrayList<Territoire> territoiresDisponibles = model.getTerritoireDispo(joueur);
-	  //  System.out.println("Territoire disponible : " + territoire.getNomTer());
+	// Test getTerritoireDispo
+	Joueur joueur = new Joueur(2, "bleu", 4); 
+	ArrayList<Territoire> territoiresDisponibles = model.getTerritoireDispo(joueur);
+	for (Territoire territoire : territoiresDisponibles) {
+		System.out.println("Territoire disponible : " + territoire.getNomTer());
 	
-
-	// Test de getTerritoireDepart
+	}
+	// Test getTerritoireDepart
 
 	ArrayList<Territoire> territoiresDepart = model.getTerritoireDepart(joueur);
 	for (Territoire territoire : territoiresDepart) {
 	    System.out.println("Territoire de départ : " + territoire.getNomTer());
 	}
 
-	// Test de getPhase
-	int phase = 1; // Remplacez par la phase que vous voulez tester (1, 2 ou 3)
+	// Test getPhase
+	int phase = 1; 
 	model.getPhase(phase);
 	}
 	
